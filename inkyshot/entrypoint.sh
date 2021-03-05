ad9ba95c27dd07c6fa5b26565231facbc5a235b4
Peter Kazazes <peter@peterk.co>
2021-03-05 15:30:03 +0000

    (fix) use python3-pillow pkg

--- inkyshot/entrypoint.sh
+++ inkyshot/entrypoint.sh
@@ -4,6 +4,6 @@ set -ex
 
 export PYTHONPATH=/opt/waveshare-sdk/python/lib:${PYTHONPATH}
 
-/usr/app/waveshare-display/display.py -i /usr/app/images/lords.jpg
+python3 /usr/app/waveshare-display/display.py -i /usr/app/images/lords.jpg
 
 balena-idle
