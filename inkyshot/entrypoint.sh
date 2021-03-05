d1c132bad209445d2678d7129275fcdeb7935c6c
Peter Kazazes <peter@peterk.co>
2021-03-05 16:22:53 +0000

    (feat) img url download

--- inkyshot/entrypoint.sh
+++ inkyshot/entrypoint.sh
@@ -4,6 +4,10 @@ set -ex
 
 export PYTHONPATH=/opt/waveshare-sdk/python/lib:${PYTHONPATH}
 
-python3 /usr/app/waveshare-display/display.py -i /usr/app/images/lords.jpg
+IMG_URL=${IMG_URL:-'https://carolineliebert.files.wordpress.com/2011/03/lords_prayer.jpg'}
+
+wget -O /tmp/image $IMG_URL
+
+python3 /usr/app/waveshare-display/display.py -i /tmp/image
 
 balena-idle
